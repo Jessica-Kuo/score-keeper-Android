ef2a2abf74b15857bad0ee2382eff2885422f4f0
Jessica-Kuo <jkuo3@binghamton.edu>
2020-07-06 19:17:40 -0400

    Added yellow card, red card, and black card counts; Added shared preferences for data persistence to all scores and cards;

--- app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -4,37 +4,81 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private int mScore1;
     private int mScore2;
+    private int mYellowCard1;
+    private int mYellowCard2;
+    private int mRedCard1;
+    private int mRedCard2;
+    private int mBlackCard1;
+    private int mBlackCard2;
     private TextView mScoreText1;
     private TextView mScoreText2;
-    static final String STATE_SCORE_1 = "Team 1 Score";
-    static final String STATE_SCORE_2 = "Team 2 Score";
+    private Button team1yCard;
+    private Button team2yCard;
+    private Button team1rCard;
+    private Button team2rCard;
+    private Button team1bCard;
+    private Button team2bCard;
+    private SharedPreferences mPreferences;
+
+    // Key for current count
+    private final String TEAM_1_KEY = "team_1_score";
+    // Key for current color
+    private final String TEAM_2_KEY = "team_2_score";
+    //Keys for red, yellow, black card
+    private final String TEAM_1_Y = "team_1_yellow";
+    private final String TEAM_2_Y = "team_2_yellow";
+    private final String TEAM_1_R = "team_1_red";
+    private final String TEAM_2_R = "team_2_red";
+    private final String TEAM_1_B = "team_1_black";
+    private final String TEAM_2_B = "team_2_black";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mScoreText1 = (TextView)findViewById(R.id.team1Score);
-        mScoreText2 = (TextView)findViewById(R.id.team2Score);
+        //initialization
+        mScoreText1 = findViewById(R.id.team1Score);
+        mScoreText2 = findViewById(R.id.team2Score);
+        team1yCard = findViewById(R.id.team1YellowCard);
+        team2yCard = findViewById(R.id.team2YellowCard);
+        team1rCard = findViewById(R.id.team1RedCard);
+        team2rCard = findViewById(R.id.team2RedCard);
+        team1bCard = findViewById(R.id.team1BlackCard);
+        team2bCard = findViewById(R.id.team2BlackCard);
+        String sharedPrefFile = "come.example.android.scorekeeper";
+        mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
 
-        if(savedInstanceState != null){
-            mScore1 = savedInstanceState.getInt(STATE_SCORE_1);
-            mScore2 = savedInstanceState.getInt(STATE_SCORE_2);
+        //grabbing values from last set
+        mScore1 = mPreferences.getInt(TEAM_1_KEY, 0);
+        mScore2 = mPreferences.getInt(TEAM_2_KEY, 0);
+        mYellowCard1 = mPreferences.getInt(TEAM_1_Y, 0);
+        mYellowCard2 = mPreferences.getInt(TEAM_2_Y, 0);
+        mRedCard1 = mPreferences.getInt(TEAM_1_R, 0);
+        mRedCard2 = mPreferences.getInt(TEAM_2_R, 0);
+        mBlackCard1 = mPreferences.getInt(TEAM_1_B, 0);
+        mBlackCard2 = mPreferences.getInt(TEAM_2_B, 0);
 
-            mScoreText1.setText(String.valueOf(mScore1));
-            mScoreText2.setText(String.valueOf(mScore2));
-        }
+        //setting those values to respective fields when the activity is loaded
+        mScoreText1.setText(String.format("%s", mScore1));
+        mScoreText2.setText(String.format("%s", mScore2));
+        team1yCard.setText(String.format("%s", mYellowCard1));
+        team2yCard.setText(String.format("%s", mYellowCard2));
+        team1rCard.setText(String.format("%s", mRedCard1));
+        team2rCard.setText(String.format("%s", mRedCard2));
+        team1bCard.setText(String.format("%s", mBlackCard1));
+        team2bCard.setText(String.format("%s", mBlackCard2));
 
     }
 
@@ -53,6 +97,26 @@ public class MainActivity extends AppCompatActivity {
             }
 // Recreate the activity for the theme change to take effect.
             recreate();
+        }
+        if(item.getItemId() == R.id.reset){
+            //reset scores
+            mScore1=0; mScore2=0;
+            mYellowCard1=0; mYellowCard2=0;
+            mRedCard1=0; mRedCard2=0;
+            mBlackCard1=0; mBlackCard2=0;
+            mScoreText1.setText(String.format("%s", mScore1));
+            mScoreText2.setText(String.format("%s", mScore2));
+            team1yCard.setText(String.format("%s", mYellowCard1));
+            team2yCard.setText(String.format("%s", mYellowCard2));
+            team1rCard.setText(String.format("%s", mRedCard1));
+            team2rCard.setText(String.format("%s", mRedCard2));
+            team1bCard.setText(String.format("%s", mBlackCard1));
+            team2bCard.setText(String.format("%s", mBlackCard2));
+
+            //clear preferences
+            SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+            preferencesEditor.clear();
+            preferencesEditor.apply();
         }
             return true;
         }
@@ -98,10 +162,60 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void yellowCard(View view) {
+        int viewID = view.getId();
+        switch(viewID){
+            case R.id.team1YellowCard:
+                mYellowCard1++;
+                team1yCard.setText(String.valueOf(mYellowCard1));
+                break;
+            case R.id.team2YellowCard:
+                mYellowCard2++;
+                team2yCard.setText(String.valueOf(mYellowCard2));
+                break;
+        }
+    }
+
+    public void redCard(View view){
+        int viewID = view.getId();
+        switch(viewID){
+            case R.id.team1RedCard:
+                mRedCard1++;
+                team1rCard.setText(String.valueOf(mRedCard1));
+                break;
+            case R.id.team2RedCard:
+                mRedCard2++;
+                team2rCard.setText(String.valueOf(mRedCard2));
+                break;
+        }
+    }
+
+    public void blackCard(View view){
+        int viewID = view.getId();
+        switch(viewID){
+            case R.id.team1BlackCard:
+                mBlackCard1++;
+                team1bCard.setText(String.valueOf(mBlackCard1));
+                break;
+            case R.id.team2BlackCard:
+                mBlackCard2++;
+                team2bCard.setText(String.valueOf(mBlackCard2));
+                break;
+        }
+    }
+
     @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        outState.putInt(STATE_SCORE_1, mScore1);
-        outState.putInt(STATE_SCORE_2, mScore2);
-        super.onSaveInstanceState(outState);
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.putInt(TEAM_1_KEY, mScore1);
+        preferencesEditor.putInt(TEAM_2_KEY, mScore2);
+        preferencesEditor.putInt(TEAM_1_Y, mYellowCard1);
+        preferencesEditor.putInt(TEAM_2_Y, mYellowCard2);
+        preferencesEditor.putInt(TEAM_1_R, mRedCard1);
+        preferencesEditor.putInt(TEAM_2_R, mRedCard2);
+        preferencesEditor.putInt(TEAM_1_B, mBlackCard1);
+        preferencesEditor.putInt(TEAM_2_B, mBlackCard2);
+        preferencesEditor.apply();
     }
 }
